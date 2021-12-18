1c314f7e8d6c3424f9b5998d07cc8a48875ba175
mouataz1 <moataz.hakkou@gmail.com>
2021-12-19 00:35:03 +0100

    section controller

---
+++ migrations/Version20211218223353.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20211218223353 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE client (id INT AUTO_INCREMENT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE contact (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(255) DEFAULT NULL, address LONGTEXT DEFAULT NULL, phone VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE message (id INT AUTO_INCREMENT NOT NULL, id_client_id INT DEFAULT NULL, contenu LONGTEXT NOT NULL, INDEX IDX_B6BD307F99DED506 (id_client_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE reponse (id INT AUTO_INCREMENT NOT NULL, id_message_id INT DEFAULT NULL, id_user_id INT DEFAULT NULL, contenu VARCHAR(255) NOT NULL, message VARCHAR(255) NOT NULL, INDEX IDX_5FB6DEC7F6F093FE (id_message_id), INDEX IDX_5FB6DEC779F37AE5 (id_user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE section (id INT AUTO_INCREMENT NOT NULL, id_user_id INT DEFAULT NULL, titre VARCHAR(255) NOT NULL, description LONGTEXT NOT NULL, contenu VARCHAR(255) NOT NULL, image VARCHAR(255) NOT NULL, bouton VARCHAR(255) NOT NULL, type VARCHAR(255) NOT NULL, INDEX IDX_2D737AEF79F37AE5 (id_user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, nom VARCHAR(255) NOT NULL, prenom VARCHAR(255) NOT NULL, username VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE message ADD CONSTRAINT FK_B6BD307F99DED506 FOREIGN KEY (id_client_id) REFERENCES client (id)');
+        $this->addSql('ALTER TABLE reponse ADD CONSTRAINT FK_5FB6DEC7F6F093FE FOREIGN KEY (id_message_id) REFERENCES message (id)');
+        $this->addSql('ALTER TABLE reponse ADD CONSTRAINT FK_5FB6DEC779F37AE5 FOREIGN KEY (id_user_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE section ADD CONSTRAINT FK_2D737AEF79F37AE5 FOREIGN KEY (id_user_id) REFERENCES user (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE message DROP FOREIGN KEY FK_B6BD307F99DED506');
+        $this->addSql('ALTER TABLE reponse DROP FOREIGN KEY FK_5FB6DEC7F6F093FE');
+        $this->addSql('ALTER TABLE reponse DROP FOREIGN KEY FK_5FB6DEC779F37AE5');
+        $this->addSql('ALTER TABLE section DROP FOREIGN KEY FK_2D737AEF79F37AE5');
+        $this->addSql('DROP TABLE client');
+        $this->addSql('DROP TABLE contact');
+        $this->addSql('DROP TABLE message');
+        $this->addSql('DROP TABLE reponse');
+        $this->addSql('DROP TABLE section');
+        $this->addSql('DROP TABLE user');
+    }
+}
--- src/Controller/SectionController.php
+++ src/Controller/SectionController.php
@@ -17,136 +17,57 @@ use Symfony\Component\Routing\Annotation\Route;
  
 class SectionController extends AbstractController
 {
+
+    /*
+     * had route katdik l la page dial sections w katreturnilk ga3 les sections li kaynin f la base se donnée
+     * */
     /**
- * @Route("/section")
- */
-
- public function index()
- {
-     $section = $this->getDoctrine()->getRepository(Section::class);
-   return $this->render('section/index.html.twig',[
-
-     ]);
-
-
- }
-
-    /**
-     * @Route("/blog", name="blog_", methods={"GET"})
+     * @Route("/section", name="section_table")
      */
-    public function showblog(SectionRepository $sectionRepository): Response
+
+    public function index()
     {
+        $section = $this->getDoctrine()->getRepository(Section::class)->findAll();
         return $this->render('section/index.html.twig', [
-            'sections' => $sectionRepository->findBy(
-                ['type'=>'blog'],
-                ['type'=>'ASC'],
-            ),
+            'sections' => $section,
         ]);
+
+
     }
 
+    /*
+     * had route katdik l la page fin tzid section jdida
+     * */
     /**
-     * @Route("/service", name="service", methods={"GET"})
+     * @Route("/addsection", name="add_section")
      */
-    public function showservice(SectionRepository $sectionRepository): Response
+    public function addSection()
     {
-        return $this->render('section/index.html.twig', [
-            'sections' => $sectionRepository->findBy(
-                ['type'=>'services'],
-                ['type'=>'ASC'],
-            ),
-        ]);
+        return $this->render('section/new.html.twig');
     }
 
-    
+    /*
+     * had route bach tmodifyi la section
+     * */
     /**
-     * @Route("/about", name="about", methods={"GET"})
+     * @Route("/updatesection", name="update_section")
      */
-    public function showabout(SectionRepository $sectionRepository): Response
+    public function updateSection()
     {
-        return $this->render('section/index.html.twig', [
-            'sections' => $sectionRepository->findBy(
-                ['type'=>'about'],
-                ['type'=>'ASC'],
-            ),
-        ]);
+        return $this->render('section/edit.html.twig');
     }
 
-    
+    /*
+     * had route tdik l show section
+     * */
     /**
-     * @Route("/project", name="project", methods={"GET"})
+     * @Route("/showsection/{id}", name="show_section")
      */
-    public function showproject(SectionRepository $sectionRepository): Response
+    public function showSection($id)
     {
-        return $this->render('section/index.html.twig', [
-            'sections' => $sectionRepository->findBy(
-                ['type'=>'project'],
-                ['type'=>'ASC'],
-            ),
-        ]);
-    }
-
-    /**
-     * @Route("/new", name="section_new", methods={"GET", "POST"})
-     */
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
-    {
-        $section = new Section();
-        $form = $this->createForm(SectionType::class, $section);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($section);
-            $entityManager->flush();
-
-            return $this->redirectToRoute('blog_', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->render('section/new.html.twig', [
-            'section' => $section,
-            'form' => $form->createView(),
-        ]);
-    }
-
-    /**
-     * @Route("/{id}", name="section_show", methods={"GET"})
-     */
-    public function show(Section $section): Response
-    {
+        $section = $this->getDoctrine()->getRepository(Section::class)->find($id);
         return $this->render('section/show.html.twig', [
             'section' => $section,
         ]);
-    }
-
-    /**
-     * @Route("/{id}/edit", name="section_edit", methods={"GET", "POST"})
-     */
-    public function edit(Request $request, Section $section, EntityManagerInterface $entityManager): Response
-    {
-        $form = $this->createForm(SectionType::class, $section);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->flush();
-
-            return $this->redirectToRoute('section_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->render('section/edit.html.twig', [
-            'section' => $section,
-            'form' => $form->createView(),
-        ]);
-    }
-
-    /**
-     * @Route("/{id}", name="section_delete", methods={"POST"})
-     */
-    public function delete(Request $request, Section $section, EntityManagerInterface $entityManager): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$section->getId(), $request->request->get('_token'))) {
-            $entityManager->remove($section);
-            $entityManager->flush();
-        }
-
-        return $this->redirectToRoute('section_index', [], Response::HTTP_SEE_OTHER);
     }
 }
